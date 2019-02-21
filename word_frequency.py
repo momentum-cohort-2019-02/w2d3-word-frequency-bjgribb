c51f6b85a8221c9cc8a8136bfe9b8f99cbe02d6c
bjgribb <bjgribb@gmail.com>
2019-02-20 22:02:26 -0500

    not sorted

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+import string
+import re
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,9 +8,88 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def normalize_text(text):
+    """
+    Given a text, lowercases it, removes all punctuation, 
+    and replaces all whitespace with normal spaces. Multiple whitespace will
+    be compressed into a single space.
+    """
+    text = text.casefold()
+    # valid_chars = re.sub(r"[^a-zA-Z]"," ",text)
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
+
+    # Remove all punctuation
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
+
+
+def print_word_freq(filename):
+    """Read in `filename` and print out the frequency of words in that file."""
+    with open(filename) as file:
+        text = file.read()
+
+    text = normalize_text(text)
+    words = []
+    for word in text.split(" "):
+        if word != '' and word not in STOP_WORDS:
+            words.append(word)
+
+    ind_words = []
+    for word in words:
+        if word not in ind_words:
+            ind_words.append(word)
+
+    word_freq = {}
+    for word in words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else: 
+            word_freq[word] = 1
+
+    # print(word_freq)
+
+    # print(sorted(word_freq.values(), reverse=True))
+
+    # for value in word_freq.items():\
+
+    # for value in word_freq.values():
+    #     sorted(value, reverse=True)
+        
+    for key, value in word_freq.items():
+        print(value, key)
+
+    for key, value in word_freq.items():
+        sorted(word_freq.items(), key=lambda x: x[1])
+        # value = sorted(value, reverse = True)
+        print(key, "|", value, "*" * value)
+
+
+    
+    # sorted_words = sorted(word_freq.values(), reverse=True)
+
+    # for sortedkey in sorted_words:
+    #     for key, value in word_freq.items():
+    #         if value == sortedkey:
+    #            sorted_word_freq[key]
+
+
+
+    # for sorted_word_freq in sorted(word_freq.values(), reverse=True):
+    #     print(sorted_word_freq)
+            
+
+
+
+            ## What now?
+    # Get a dictionary of word frequencies and print it out
+
+
 
 
 if __name__ == "__main__":
